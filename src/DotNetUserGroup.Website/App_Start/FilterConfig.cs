f8bb5085ca4409795d3a54df521169b81ab509da
Amir Barylko <amir@barylko.com>
2012-07-27 16:49:36 -0500

    Added hardcoded events

--- src/DotNetUserGroup.Website/App_Start/FilterConfig.cs
+++ src/DotNetUserGroup.Website/App_Start/FilterConfig.cs
@@ -1,7 +1,6 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace DotNetUserGroup.Website
+namespace DotNetUserGroup.Website.App_Start
 {
     public class FilterConfig
     {
--- src/DotNetUserGroup.Website/App_Start/RouteConfig.cs
+++ src/DotNetUserGroup.Website/App_Start/RouteConfig.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace DotNetUserGroup.Website
+namespace DotNetUserGroup.Website.App_Start
 {
     public class RouteConfig
     {
@@ -18,12 +14,6 @@ namespace DotNetUserGroup.Website
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
