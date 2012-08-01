df81556fc33bee4f8c185e036f671218ad2fd802
Amir Barylko <amir@barylko.com>
2012-08-01 12:28:10 -0500

    Added code to check for EventBrite variables in the system environment when is DEBUG mode, otherwise uses the configuration

--- src/DotNetUserGroup.Website/Models/EventBriteRepository.cs
+++ src/DotNetUserGroup.Website/Models/EventBriteRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Web.Configuration;
 using EasyHttp.Http;
 
@@ -7,21 +8,11 @@ namespace DotNetUserGroup.Website.Models
 {
     public class EventBriteRepository : IEventRepository
     {
+        private const string url = "https://www.eventbrite.com/json/";
+
         public IEnumerable<UserGroupEvent> All()
         {
-            var http = new HttpClient();
-            http.Request.Accept = HttpContentTypes.ApplicationJson;
-            const string url = "https://www.eventbrite.com/json/user_list_events";
-
-            var query = new
-                            {
-                                app_key = WebConfigurationManager.AppSettings["EB_APP_KEY"], 
-                                user_key = WebConfigurationManager.AppSettings["EB_USER_KEY"]
-                            };
-
-            var response = http.Get(url, query);
-
-            var eventList = response.DynamicBody.events;
+            var eventList = Request("/user_list_events", LoadConfiguration()).DynamicBody.events;
 
             var result = new List<UserGroupEvent>();
 
@@ -39,6 +30,38 @@ namespace DotNetUserGroup.Website.Models
             }
 
             return result;
+        }
+
+        private static HttpResponse Request(string method, object configuration)
+        {
+            var http = new HttpClient();
+
+            http.Request.Accept = HttpContentTypes.ApplicationJson;
+
+            var response = http.Get(url + "/" + method, configuration);
+
+            return response;
+        }
+
+        private static object LoadConfiguration()
+        {
+            var section = WebConfigurationManager.AppSettings;
+
+            var query = new
+                            {
+                                app_key = section["EB_APP_KEY"],
+                                user_key = section["EB_USER_KEY"]
+                            };
+
+#if DEBUG
+            query = new
+            {
+                app_key = Environment.GetEnvironmentVariable("EB_APP_KEY"),
+                user_key = Environment.GetEnvironmentVariable("EB_USER_KEY")
+            };
+#endif
+
+            return query;
         }
     }
 }
