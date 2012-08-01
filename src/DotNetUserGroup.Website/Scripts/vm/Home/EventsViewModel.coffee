269d6bc54b468a24699376a5a7b874ae43c5c567
Amir Barylko <amir@barylko.com>
2012-07-31 23:13:28 -0500

    Added date parsing into the view model

--- src/DotNetUserGroup.Website/Scripts/vm/Home/EventsViewModel.coffee
+++ src/DotNetUserGroup.Website/Scripts/vm/Home/EventsViewModel.coffee
@@ -2,7 +2,15 @@ class EventsViewModel
 
 	constructor: ->
 		@events = ko.observableArray()
-		$.getJSON '../api/events', (data) => @events(data[0..2])
+		@nextEvent = ko.observable()
+
+		$.getJSON '../api/events', (data) =>
+			es = ({Title: e.Title, Date: @parseDate(e.Date)} for e in data[0..2])
+			@events(es)
+
+	parseDate: (d) ->
+		str = new Date(Date.parse(d)).toString()
+		str.split(' ')[1..2].join(' ')
 
 $ ->
 	ko.applyBindings(new EventsViewModel(), document.getElementById('events'))
--- src/DotNetUserGroup.Website/Scripts/vm/Home/EventsViewModel.js
+++ src/DotNetUserGroup.Website/Scripts/vm/Home/EventsViewModel.js
@@ -8,10 +8,31 @@
     function EventsViewModel() {
       var _this = this;
       this.events = ko.observableArray();
+      this.nextEvent = ko.observable();
       $.getJSON('../api/events', function(data) {
-        return _this.events(data.slice(0, 3));
+        var e, es;
+        es = (function() {
+          var _i, _len, _ref, _results;
+          _ref = data.slice(0, 3);
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            e = _ref[_i];
+            _results.push({
+              Title: e.Title,
+              Date: this.parseDate(e.Date)
+            });
+          }
+          return _results;
+        }).call(_this);
+        return _this.events(es);
       });
     }
+
+    EventsViewModel.prototype.parseDate = function(d) {
+      var str;
+      str = new Date(Date.parse(d)).toString();
+      return str.split(' ').slice(1, 3).join(' ');
+    };
 
     return EventsViewModel;
 
