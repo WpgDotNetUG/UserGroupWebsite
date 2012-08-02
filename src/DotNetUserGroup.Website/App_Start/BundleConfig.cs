5841f7e6b61a9b6645c256b49ac6404ae2f63673
Amir Barylko <amir@barylko.com>
2012-08-01 19:17:03 -0500

    Added helper for sponsors, more debug info for settings

--- src/DotNetUserGroup.Website/App_Start/BundleConfig.cs
+++ src/DotNetUserGroup.Website/App_Start/BundleConfig.cs
@@ -27,6 +27,7 @@ namespace DotNetUserGroup.Website.App_Start
 
             bundles.Add(new StyleBundle("~/Content/css")
                             .Include("~/Content/bootstrap.css",
+                                     "~/Content/bootstrap-responsive.css",
                                      "~/Content/font-awesome.css",
                                      "~/Content/site.css"));
 
