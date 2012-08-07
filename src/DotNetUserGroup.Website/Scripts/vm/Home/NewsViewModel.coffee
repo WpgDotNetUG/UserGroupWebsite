ff2603aeac82288904e9ade2a79d3ffb99fef3ab
Amir Barylko <amir@barylko.com>
2012-08-06 23:59:46 -0500

    Changed ViewModel to parse the date

--- src/DotNetUserGroup.Website/Scripts/vm/Home/NewsViewModel.coffee
+++ src/DotNetUserGroup.Website/Scripts/vm/Home/NewsViewModel.coffee
@@ -2,5 +2,14 @@ class window.NewsViewModel
 
 	constructor: (limit = -1) ->
 		@news = ko.observableArray()
-		$.getJSON '../api/news', (data) => @news(data[0..(if limit == -1 then -1 else limit - 1)])
+		$.getJSON '../api/news', (data) => 
+			es = ({
+				Title: e.Title 
+				Date: @parseDate(e.Date)
+				Body: e.Body
+			} for e in data[0..(if limit == -1 then -1 else limit - 1)])
+			@news(es)
 
+	parseDate: (d) ->
+		str = new Date(Date.parse(d)).toString()
+		str.split(' ')[1..2].join(' ')
--- src/DotNetUserGroup.Website/Scripts/vm/Home/NewsViewModel.js
+++ src/DotNetUserGroup.Website/Scripts/vm/Home/NewsViewModel.js
@@ -11,9 +11,30 @@
       }
       this.news = ko.observableArray();
       $.getJSON('../api/news', function(data) {
-        return _this.news(data.slice(0, (limit === -1 ? -1 : limit - 1) + 1 || 9e9));
+        var e, es;
+        es = (function() {
+          var _i, _len, _ref, _results;
+          _ref = data.slice(0, (limit === -1 ? -1 : limit - 1) + 1 || 9e9);
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            e = _ref[_i];
+            _results.push({
+              Title: e.Title,
+              Date: this.parseDate(e.Date),
+              Body: e.Body
+            });
+          }
+          return _results;
+        }).call(_this);
+        return _this.news(es);
       });
     }
+
+    NewsViewModel.prototype.parseDate = function(d) {
+      var str;
+      str = new Date(Date.parse(d)).toString();
+      return str.split(' ').slice(1, 3).join(' ');
+    };
 
     return NewsViewModel;
 
