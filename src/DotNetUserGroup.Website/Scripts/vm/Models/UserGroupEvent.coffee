9647fd64cc46e47f8786f87334a99ea4a766c694
Amir Barylko <amir@barylko.com>
2013-09-07 07:58:15 -0600

    Changed icon styles, added end date information

--- src/DotNetUserGroup.Website/Scripts/vm/Models/UserGroupEvent.coffee
+++ src/DotNetUserGroup.Website/Scripts/vm/Models/UserGroupEvent.coffee
@@ -4,13 +4,17 @@ class window.UserGroupEvent
         @date    = ko.observable @formatDate(event.Date)
         @venue   = ko.observable event.Venue
         @address = ko.observable event.Address
-        @time    = ko.observable @formatTime(event.Date)
+        @time    = ko.observable @formatTime(event)
         @url     = ko.observable "http://www.eventbrite.ca/event/#{event.Id}"
         @title   = ko.observable event.Title
         @description = ko.observable event.Description?.split('\n')[0..0].join('\n') + "..."
 
     formatDate: (d) -> d && Date.parse(d).toString('dddd, MMMM dd, yyyy')
-    formatTime: (d) -> d && Date.parse(d).toString('HH:mm')
+    formatTime: (e) -> 
+        return unless e.EndDate
+        start = Date.parse(e.Date).toString('HH:mm')
+        end   = Date.parse(e.EndDate).toString('HH:mm')
+        "#{start} to #{end}"
 
     @Empty: ->
         new UserGroupEvent
---
+++ src/DotNetUserGroup.Website/Scripts/vm/Models/UserGroupEvent.js
@@ -0,0 +1,44 @@
+(function() {
+  window.UserGroupEvent = (function() {
+    function UserGroupEvent(event) {
+      var _ref;
+
+      this.date = ko.observable(this.formatDate(event.Date));
+      this.venue = ko.observable(event.Venue);
+      this.address = ko.observable(event.Address);
+      this.time = ko.observable(this.formatTime(event));
+      this.url = ko.observable("http://www.eventbrite.ca/event/" + event.Id);
+      this.title = ko.observable(event.Title);
+      this.description = ko.observable(((_ref = event.Description) != null ? _ref.split('\n').slice(0, 1).join('\n') : void 0) + "...");
+    }
+
+    UserGroupEvent.prototype.formatDate = function(d) {
+      return d && Date.parse(d).toString('dddd, MMMM dd, yyyy');
+    };
+
+    UserGroupEvent.prototype.formatTime = function(e) {
+      var end, start;
+
+      if (!e.EndDate) {
+        return;
+      }
+      start = Date.parse(e.Date).toString('HH:mm');
+      end = Date.parse(e.EndDate).toString('HH:mm');
+      return "" + start + " to " + end;
+    };
+
+    UserGroupEvent.Empty = function() {
+      return new UserGroupEvent({
+        Date: null,
+        Venue: null,
+        Title: null,
+        Status: null,
+        EndDate: null
+      });
+    };
+
+    return UserGroupEvent;
+
+  })();
+
+}).call(this);
