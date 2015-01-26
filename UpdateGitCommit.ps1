e6596034a6ad55778497991fe328fa21875b06ca
Tyler Doerksen <tylergd@outlook.com>
2015-01-26 15:08:33 -0600

    updating script

--- UpdateGitCommit.ps1
+++ UpdateGitCommit.ps1
@@ -1,4 +1,10 @@
-﻿$webConfig = "$env:DEPLOYMENT_TARGET\Web.config"
+﻿Write-Output "DEPLOYMENT_TARGET: '$env:DEPLOYMENT_TARGET'"
+
+Write-Output "SVC_COMMIT_ID: '$env:SVC_COMMIT_ID'"
+
+
+
+$webConfig = "$env:DEPLOYMENT_TARGET\Web.config"
 
 $doc = (Get-Content $webConfig) -as [Xml]
 
@@ -7,3 +13,5 @@ $node = $doc.configuration.appSettings.add | where Key -EQ "appharbor.commit_id"
 $node.Value = $env:SVC_COMMIT_ID
 
 $doc.Save($webConfig)
+
+Write-Output "Completed App setting update"
