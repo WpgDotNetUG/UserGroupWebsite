cf30dd776c755be4de77c53fc3d9f785eae9ab92
Quinn Wilson <git@midword.org>
2015-07-19 20:43:48 -0500

    add slack invitation form

--- src/DotNetUserGroup.Website/Controllers/HomeController.cs
+++ src/DotNetUserGroup.Website/Controllers/HomeController.cs
@@ -4,9 +4,30 @@ namespace DotNetUserGroup.Website.Controllers
 {
     public class HomeController : Controller
     {
+		private readonly ISlackInc _slackInc;
+
+		public HomeController(ISlackInc slackInC)
+	    {
+			_slackInc = slackInC;
+	    }
         public ActionResult Index()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		public ActionResult Signup(string email)
+		{
+			var res = _slackInc.Invite(email);
+			if (res.Ok)
+			{
+				ViewBag.Success = "Invitation sent!";
+			}
+			else
+			{
+				ViewBag.Error = res.ErrorMessage;
+			}
+			return View("Index");
+		}
+	}
 }
